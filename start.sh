a7cba1b45248db2d006a4e4f29819b1150515baa
sednafun <166916681+sednafun@users.noreply.github.com>
2026-06-01 18:11:47 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# Установка Tesseract
 apt-get update
 apt-get install -y tesseract-ocr
+
+# Установка зависимостей
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Запуск бота
 python bot.py
